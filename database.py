2b4f389f7d0cb3074164b01e06140e694a335096
Yusuf Rafif Karback <yusufkarback@Yusufs-MacBook-Pro.local>
2021-12-27 22:41:41 +0700

    membuat fungsi insert

--- database.py
+++ database.py
@@ -1,13 +1,21 @@
 import mysql.connector
+import datetime
 
 mydb = mysql.connector.connect(
     host="localhost",
     user="root",
     password="root",
     port=8889,
+    database="mydatabase"
 )
 mycursor = mydb.cursor()
-mycursor.execute("SHOW DATABASES")
 
-for i in mycursor:
-    print(i)
+nama = input('masukan nama buku: ')
+date = datetime.datetime.now()
+
+sql = "INSERT INTO books(nama_buku, waktu_masuk) VALUES ('{}', '{}')".format(nama, date)
+mycursor.execute(sql)
+
+mydb.commit()
+
+print(mycursor.rowcount, 'record inserted')
